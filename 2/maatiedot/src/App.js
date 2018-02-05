f8378ecb3564e2ea6d89f0967a4c24dd7189c715
lainahai <jani.takkinen@cs.helsinki.fi>
2018-02-05 22:00:14 +0200

    2.12

---
+++ 2/maatiedot/src/App.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      countries: [],
+      filter: ''
+    }
+  }
+
+  componentWillMount() {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(
+        response => {
+          this.setState({countries: response.data})
+        }
+      )
+  }
+
+  handleFilterChange = (event) => {
+    this.setState({filter: event.target.value})
+  }
+
+  render() {
+    const showCountries = this.state.countries.filter( (country) => (
+      country.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    ))
+
+    if(showCountries.length === 1){
+      return (
+        <div>
+          <FilterInput value={this.state.filter} handleChange={this.handleFilterChange.bind(this)} />
+          <SingleCountry country={showCountries[0]}/> 
+        </div>
+      )
+    }
+
+    if(showCountries.length > 10){
+      return (
+        <div>
+          <FilterInput value={this.state.filter} handleChange={this.handleFilterChange.bind(this)} />
+          <p>Too many results, please use a more specific filter.</p>
+        </div>
+      )
+    }
+
+    return (
+      <div>
+        <FilterInput value={this.state.filter} handleChange={this.handleFilterChange.bind(this)} />
+        <Countries countries={showCountries} />
+      </div>
+    )
+  }
+}
+
+const FilterInput = (props) => {
+  return (
+    <div>
+      Filter by name: 
+      <input value={props.value} onChange={props.handleChange} />
+    </div>
+  )
+}
+
+const Countries = ({ countries }) => {
+  return (
+    <div>
+      <ul>
+        {countries.map(country => <li>{country.name}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+const SingleCountry = ({country}) => {
+
+  return (
+    <div>
+      <h2>{country.name} {country.nativeName}</h2>
+      <p>Capital: {country.capital}</p>
+      <p>Population: {country.population}</p>
+      <div>
+        <img alt='Flag of a country' src={country.flag}/>
+      </div>
+    </div>
+  )
+}
+
+export default App
