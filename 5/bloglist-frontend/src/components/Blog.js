3b694e7bde9515e711a4c1ee210fc2b158450965
lainahai <jani.takkinen@cs.helsinki.fi>
2018-03-04 22:08:48 +0200

    5.6

--- 5/bloglist-frontend/src/components/Blog.js
+++ 5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,61 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.blog = props.blog
+    this.state = {
+      visible: false
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible })
+  }
+
+  render() {
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      marginBottom: 5,
+      background: '#F9F9F9'
+    }
+
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+
+    return (
+      <div style={blogStyle} onClick={this.toggleVisibility}>
+        {this.blog.title} {this.blog.author}
+        <div style={showWhenVisible}>
+          <table>
+            <tbody>
+              <tr>
+                <td>Id: </td>
+                <td>{this.blog._id}</td>
+              </tr>
+              <tr>
+                <td>URL</td>
+                <td>
+                  <a href={this.blog.url}>
+                    {this.blog.url}
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td>Likes</td>
+                <td>{this.blog.likes}</td>
+                <td><button>Like</button></td>
+              </tr>
+              <tr>
+                <td>Added by </td>
+                <td>{this.blog.user.name}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Blog
