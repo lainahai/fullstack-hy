9d4a7a17d29e7c118311b632668e59b0aeab3d37
lainahai <jani.takkinen@cs.helsinki.fi>
2018-03-11 20:31:47 +0200

    6.1

---
+++ 6/redux-anecdotes-v2/.eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true,
+        "browser": true
+    },
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaVersion": 6
+    },/*
+    "ecmaFeatures": {
+        "jsx": true
+    },*/
+    "plugins": ["react"],
+    "extends": [ "eslint:recommended", "plugin:react/recommended"],
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "eqeqeq": "error",
+        "no-console": 0,
+        "react/prop-types": 0
+    },
+    "globals": {
+        "test": true,
+        "expect": true,
+        "describe": true
+    },
+    "parser": "babel-eslint"
+};
